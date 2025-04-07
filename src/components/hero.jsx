1dce606edad2c5c50602a0b153cdfd04105aa12b
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-04-07 03:21:57 +0100

    feat: Add AOS library for animations; implement fade effects in Hero component

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,8 +1,10 @@
+import Aos from "aos";
 import hero from "../assets/omotolani.jpg"; // Importing the hero image
 import { LiaGithub, LiaLinkedin } from "react-icons/lia";
 import { RiMediumFill } from "react-icons/ri";
 import { SiTableau } from "react-icons/si";
 
+Aos.init();
 const Hero = () => {
   return (
     <section
@@ -11,26 +13,41 @@ const Hero = () => {
     >
       {/* Left Content */}
       <div className="w-full md:w-1/2 flex flex-col gap-4 justify-center items-center md:items-start p-6  ">
-        <span>Hey, I'm</span>
-        <h1 className="text-3xl md:text-4xl font-bold text-primary md:text-left text-center leading-tight font-sora whitespace-nowrap">
+        <span data-aos="fade-up-right">Hey, I'm</span>
+        <h1
+          data-aos="fade-up-right"
+          className="text-3xl md:text-4xl font-bold text-primary md:text-left text-center leading-tight font-sora whitespace-nowrap"
+        >
           Omotolani Kehide-Osems
         </h1>
-        <p className="text-gray-700 text-sm md:text-base leading-relaxed max-w-md md:text-left text-center mt-1">
+        <p
+          data-aos="fade-up-right"
+          className="text-gray-700 text-sm md:text-base leading-relaxed max-w-md md:text-left text-center mt-1"
+        >
           ~ ML Engineer | Data Scientist | Software Engineer
         </p>
         <div className="flex gap-2 mt-2 justify-center md:justify-start">
           <a href="https://www.linkedin.com/in/omotolani-kehinde-osems-98140818b/">
-            <button className="bg-primary text-white p-3 rounded-full hover:shadow-md transition duration-300">
+            <button
+              className="bg-primary text-white p-3 rounded-full hover:shadow-md transition duration-300"
+              data-aos="fade-up-right"
+            >
               <LiaLinkedin className="size-6 bg-primary text-white rounded-full" />
             </button>
           </a>
           <a href="https://medium.com/@omotolaniosems ">
-            <button className="bg-primary text-white p-3 rounded-full hover:shadow-md transition duration-300">
+            <button
+              className="bg-primary text-white p-3 rounded-full hover:shadow-md transition duration-300"
+              data-aos="fade-up-right"
+            >
               <RiMediumFill className="size-6 bg-primary text-white rounded-full" />
             </button>
           </a>
           <a href="https://github.com/omotolani12">
-            <button className="bg-primary text-white p-3 rounded-full hover:shadow-md transition duration-300">
+            <button
+              className="bg-primary text-white p-3 rounded-full hover:shadow-md transition duration-300"
+              data-aos="fade-up-right"
+            >
               <LiaGithub className="size-6 bg-primary text-white rounded-full" />
             </button>
           </a>
@@ -48,6 +65,7 @@ const Hero = () => {
           src={hero}
           alt="Hero"
           className="md:w-3/5 w-1/2 max-w-md mx-auto md:h-3/5 h-1/2 rounded-full shadow-lg object-cover"
+          data-aos="fade-up-left"
         />
       </div>
     </section>
